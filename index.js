add3741e71fc96254514a9f8bfd2e19581421003
AdamHlg00 <adamoliver.holgersson@gmail.com>
2023-04-07 18:24:09 +0200

    Connected the database to the program and created a file for the album model.

--- index.js
+++ index.js
@@ -2,10 +2,30 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// Imports album model
+const AlbumModel = require('./models/albumModel')
+
+const password = 'ixR3RN7ZL2b9nlN7'
+
+const mongoose = require('mongoose')
+mongoose.set('strictQuery', false)
+const mongoDB = `mongodb+srv://user:${password}@fullstacklab1.qrmbdib.mongodb.net/?retryWrites=true&w=majority`
+
+main().catch(err => console.log(err))
+async function main() {
+  await mongoose.connect(mongoDB)
+}
+
+// Will log if the database is connected
+mongoose.connection.on('connected', () => {
+  console.log('Mongoose is connected')
+})
+
 app.listen(port, () => {
   console.log('Server is listening on port: ' + port)
 })
 
-app.get('/', function (req, res) {
+app.get('/', async function (req, res) {
+  console.log(await AlbumModel.find())
   res.json('The server is up!')
 })
